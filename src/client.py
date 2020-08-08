91692ab8094c1413f06db1c536132062ed607322
Andree Avalos <aavalosoto@gmail.com>
2020-08-08 15:24:32 -0600

    Read List
    Se implemento leer lista con token generado

--- src/client.py
+++ src/client.py
@@ -1,6 +1,3 @@
-''' Importamos la libreria zeep la cual nos brindara soporte 
-    para poder usar peticiones SOAP '''
-from zeep import Client
 import requests
 import json
 
@@ -8,33 +5,34 @@ class Cliente():
     def __init__(self):
         #URL proporcionado con todos los servicios
         self.api = "https://api.softwareavanzado.world/index.php"
-        self.url = self.api+ "?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=soap&wsdl"
+        self.url = self.api+"?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=hal"
         self.url_token = self.api+"?option=token&api=oauth2"
         #objeto tipo cliente referente a la libreria zeep
-        self.client = None
+        self.headers = None
 
-    def getToken(self):
+    def authenticate(self):
         data = {"grant_type": "client_credentials","client_id":"sa", "client_secret":"fb5089840031449f1a4bf2c91c2bd2261d5b2f122bd8754ffe23be17b107b8eb103b441de3771745"}
         request = requests.post(self.url_token, data = data)
         res = json.loads(request.text)
-        return res["token_type"] + " " + res["access_token"] 
-
-
-    def authenticate(self, token = None, username = None, password = None):
-        Client(self.url)
-
-
-    #Metodo utilizado para crear contacto 
-    def create(self,name="default",catid = 1, published = 1):
-        '''Name:string(obligatorio), catid:int(opcional), published:int(opcional)'''
-        #peticion para crear un contacto en el servidor SOAP
-        self.client.service.create(name= name, catid=catid, published=published)
-
+        token = 'Bearer {0}'.format(res["access_token"])
+        self.headers = {
+            'Authorization': token,
+            'Content-Type': 'application/xml'
+            }
 
     #Metodo para listar contactos referentes al servidor
     def readList(self):
-        #peticion al servicion readlList en el servidor SOAP
-        result = self.client.service.readList()
+        payload = '''<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\" 
+                                    xmlns:adm=\"https://api.softwareavanzado.world/media/redcore/webservices/joomla/administrator.contact.1.0.0.wsdl\">\r\n   
+                                    <soap:Header/>\r\n   
+                                    <soap:Body>\r\n      
+                                    <adm:readList>\r\n
+                                    <filterSearch>201408580</filterSearch>\r\n\r\n   
+                                    <limit>0</limit>\r\n\r\n   
+                                    </adm:readList>\r\n   
+                                    </soap:Body>\r\n</soap:Envelope>'''
+        response = requests.request("GET", self.url, headers=self.headers, data = payload)
+        result = json.loads(response.text)
         #recorremos el resultado para imprimirlo en consola
-        for contact in result:
+        for contact in result["_embedded"]["item"]:
             print("ID:", contact["id"],"Nombre:",contact["name"])
